d3f6a57f72558cdf9f1ce98d1e9b077510909eee
André Simões <alexandr3@gmail.com>
2015-10-15 14:55:42 +0100

    Added ubuntu pentaho bi server EE template

---
+++ trusty-biserver/provision.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+# Turn off ubuntu interactive mode
+export DEBIAN_FRONTEND=noninteractive
+
+# Set the timezone
+echo "Europe/Lisbon" > /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
+
+# Set the kb layout
+sed -i 's/"pc105"/"macbook79"/g' /etc/default/keyboard
+sed -i 's/"us"/"pt"/g' /etc/default/keyboard
+apt-get install -y console-common
+install-keymap pt
+
+# Disable IPV6 and set swappiness to 1
+grep net.ipv6.conf.all.disable_ipv6 /etc/sysctl.conf || (echo "net.ipv6.conf.all.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf)
+grep net.ipv6.conf.default.disable_ipv6 /etc/sysctl.conf || (echo "net.ipv6.conf.default.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf)
+grep net.ipv6.conf.lo.disable_ipv6 /etc/sysctl.conf || (echo "net.ipv6.conf.lo.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf)
+grep vm.swappiness /etc/sysctl.conf || (echo "vm.swappiness = 1" | sudo tee -a /etc/sysctl.conf)
+sysctl -p
+
+# Add Java Repo
+add-apt-repository -y ppa:webupd8team/java > /dev/null 2>&1
+
+# Update and set defaults
+apt-get update > /dev/null
+sh -c "echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections"
+sh -c "echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections"
+
+# Install Java
+apt-get install -y oracle-java7-installer unzip > /dev/null 2>&1
---
+++ trusty-biserver/provision_pentaho_ee.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+
+pentaho_version=$1
+
+echo "Provisioning Pentaho $pentaho_version"
+
+# Unpack the zip files
+sudo -u vagrant unzip /vagrant-files/biserver-ee-$pentaho_version-dist.zip -d /home/vagrant/
+sudo -u vagrant unzip /vagrant-files/paz-plugin-ee-$pentaho_version-dist.zip -d /home/vagrant/
+sudo -u vagrant unzip /vagrant-files/pir-plugin-ee-$pentaho_version-dist.zip -d /home/vagrant/
+sudo -u vagrant unzip /vagrant-files/pdd-plugin-ee-$pentaho_version-dist.zip -d /home/vagrant/
+
+# Install the BI server
+sudo -u vagrant java -DINSTALL_PATH=/home/vagrant/pentaho -jar /home/vagrant/biserver-ee-$pentaho_version/installer.jar -options-system
+
+# Install the plugins
+sudo -u vagrant java -DINSTALL_PATH=/home/vagrant/pentaho-plugins -jar /home/vagrant/paz-plugin-ee-$pentaho_version/installer.jar -options-system
+sudo -u vagrant java -DINSTALL_PATH=/home/vagrant/pentaho-plugins -jar /home/vagrant/pir-plugin-ee-$pentaho_version/installer.jar -options-system
+sudo -u vagrant java -DINSTALL_PATH=/home/vagrant/pentaho-plugins -jar /home/vagrant/pdd-plugin-ee-$pentaho_version/installer.jar -options-system
+rm /home/vagrant/pentaho-plugins/*eula*
+sudo -u vagrant mv /home/vagrant/pentaho-plugins/* /home/vagrant/pentaho/biserver-ee/pentaho-solutions/system
+
+# Cleanup
+rm -rf /home/vagrant/biserver-ee-$pentaho_version
+rm -rf /home/vagrant/paz-plugin-ee-$pentaho_version
+rm -rf /home/vagrant/pir-plugin-ee-$pentaho_version
+rm -rf /home/vagrant/pdd-plugin-ee-$pentaho_version
+
+# Install PostgreSQL
+apt-get install -y postgresql > /dev/null 2>&1
+
+# Setup the PostgreSQL repos
+sudo -u postgres psql < /home/vagrant/pentaho/biserver-ee/data/postgresql/create_repository_postgresql.sql > /dev/null 2>&1
+sudo -u postgres psql < /home/vagrant/pentaho/biserver-ee/data/postgresql/create_jcr_postgresql.sql > /dev/null 2>&1
+sudo -u postgres psql < /home/vagrant/pentaho/biserver-ee/data/postgresql/create_quartz_postgresql.sql > /dev/null 2>&1
+
+# configure postgres to accept remote connections
+cat >> /etc/postgresql/9.3/main/pg_hba.conf <<EOF
+# Accept all IPv4 connections
+host    all         all         0.0.0.0/0             md5
+EOF
+
+service postgresql restart
+
+# Start Pentaho
+rm /home/vagrant/pentaho/biserver-ee/promptuser.sh
+sudo -u vagrant sh /home/vagrant/pentaho/biserver-ee/start-pentaho.sh
