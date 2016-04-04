c1c213f8f27d34a787bce4cbab868bf23416e035
Andre Simoes <andre.simoes@xpand-it.com>
2016-04-04 11:56:19 +0100

    Added spark config, made CDH version variable

---
+++ trusty-cdh5/scripts/spark.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+# install base spark packages
+apt-get install spark-core spark-history-server spark-python
--- trusty-cdh5/scripts/yarn.sh
+++ trusty-cdh5/scripts/yarn.sh
@@ -1,9 +1,24 @@
 #! /bin/bash
 
+HOSTNAME=$(hostname -f)
+CLOUDERA_CDH_VERSION=$1
+
 # Add Cloudera Repo
 wget http://archive.cloudera.com/cdh5/one-click-install/trusty/amd64/cdh5-repository_1.0_all.deb
 dpkg -i cdh5-repository_1.0_all.deb
 rm cdh5-repository_1.0_all.deb
+
+wget https://archive.cloudera.com/cdh5/ubuntu/trusty/amd64/cdh/archive.key -O archive.key
+sudo apt-key add archive.key
+
+cat >> /etc/apt/preferences.d/cloudera.pref <<EOF
+Package: *
+Pin: release o=Cloudera, l=Cloudera
+Pin-Priority: 501
+EOF
+
+# make the cloudera CDH version fixed
+sed -i "s/trusty-cdh5/trusty-cdh$CLOUDERA_CDH_VERSION/g" /etc/apt/sources.list.d/cloudera-cdh5.list
 
 # Update and set defaults
 apt-get update > /dev/null
@@ -21,14 +36,14 @@ for x in `cd /etc/init.d ; ls hadoop-hdfs-*` ; do sudo service $x start ; done
 sh /usr/lib/hadoop/libexec/init-hdfs.sh
 sudo -u hdfs hadoop fs -mkdir -p /user/vagrant
 sudo -u hdfs hadoop fs -chown vagrant /user/vagrant
-sudo -u hdfs hadoop fs -mkdir -p /opt 
+sudo -u hdfs hadoop fs -mkdir -p /opt
 sudo -u hdfs hadoop fs -chmod -R 1777 /opt
 
 # Start YARN services
 service hadoop-yarn-resourcemanager start
-service hadoop-yarn-nodemanager start 
+service hadoop-yarn-nodemanager start
 service hadoop-mapreduce-historyserver start
 
 # Set hadoop services to listen on the appropriate hostname
-sed -i 's/localhost/trusty-cdh5/g' /etc/hadoop/conf/core-site.xml
-sed -i 's/localhost/trusty-cdh5/g' /etc/hadoop/conf/mapred-site.xml
+sed -i "s/localhost/$HOSTNAME/g" /etc/hadoop/conf/core-site.xml
+sed -i "s/localhost/$HOSTNAME/g" /etc/hadoop/conf/mapred-site.xml
