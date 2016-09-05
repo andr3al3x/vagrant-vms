75d9d438916babc485e6272a54be676fb6deb7fb
André Simões <alexandr3@gmail.com>
2016-09-05 16:28:20 +0100

    Fixed R provisioning

--- centos-hdp/scripts/rlang.sh
+++ centos-hdp/scripts/rlang.sh
@@ -8,9 +8,10 @@ yum -y update
 yum -y install R
 
 # Install the rJava package
+mkdir -p /opt/R/packages
 R CMD javareconf JAVA_HOME=/usr/jdk64/jdk1.8.0_60
 Rscript -e 'install.packages("rJava", lib="/opt/R/packages", repos="http://cran.r-project.org")'
 
 # set the R environment settings
 echo "export R_HOME=/usr/lib64/R" >> /etc/profile.d/rlang.sh
-echo "export R_LIBS_HOME=/opt/R/packages" >> /etc/profile.d/rlang.sh
+echo "export R_LIBS_USER=/opt/R/packages" >> /etc/profile.d/rlang.sh
